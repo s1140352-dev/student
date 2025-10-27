93d2b4e24b341f2e63a2e3cdfedf51ff0dfe7003
s1140352-dev <s1140352@dmhs.kh.edu.tw>
2025-10-27 16:31:52 +0800

    Add files via upload

---
+++ hw2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int age = 16;
+    double height = 159;
+    std::string name = "老登";
+    bool is_sudent = false;
+    std::cout << "身高:"<< height ::endl;
+    std::cout << "年齡:"<< age::endl;
+    std::cout << "姓名:"<< name::endl;
+    std::cout << "是否為學生:" is_sudent << std ::endl;
+
+    return 0;
+}
